71d19097b719652108dead30f41a12251bb4a925
DtheMakeeeR <danilgol45@mail.ru>
2026-02-21 23:13:39 +0300

    start

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,23 +2,38 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+	if (value < 2) return false;
+	for (uint64_t i = 2; i*i <= value; i++)
+	{
+		if (value % i == 0) return false;
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+	uint64_t res = 0;
+	for (uint64_t i = 0; i < n; i++)
+	{
+		res = nextPrime(res);
+	}
+	return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+	uint64_t res = value+1;
+	while (!checkPrime(res))
+	{
+		res++;
+	}
+	return res;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+	uint64_t res = 0;
+	for (uint64_t i = 2; i < hbound; i++)
+	{
+		if (checkPrime(i)) res += i;
+	}
+	return res;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 // Copyright 2025 UNN-CS
 #include "alg.h"
-
 int main() {
     return 0;
 }
